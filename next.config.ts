ff4fa5f44de78003a0f625a3dd7268a40b227d14
rushikesh0022 <rushionline18@gmail.com>
2025-09-07 14:50:59 +0530

    edited static

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
